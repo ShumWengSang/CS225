3e671fffb1f7ccafc9de34112e83817c89d06b5f
ShumWengSang <ballwallcallall@gmail.com>
2019-05-15 17:48:46 -0700

    Assignment

---
+++ Assignment 1/splitter-files/splitter.h
@@ -0,0 +1,25 @@
+#ifndef SPLITTER_H
+#define SPLITTER_H
+
+#ifdef __cplusplus
+extern "C" {  //only visible by C++ compiler
+#endif
+
+#include <stddef.h> /* size_t definition */
+
+enum {
+    E_BAD_SOURCE = 1,
+    E_BAD_DESTINATION,
+    E_NO_MEMORY,
+    E_NO_ACTION,
+    E_SMALL_SIZE
+};
+
+int SplitFile(char const *  filename,  char const * output, size_t size);
+int JoinFiles(char** filenames, int num_files, char const * output);
+
+#endif 
+
+#ifdef __cplusplus
+} //only visible by C++ compiler
+#endif
